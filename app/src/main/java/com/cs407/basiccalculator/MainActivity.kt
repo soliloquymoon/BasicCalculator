3716a922aba0121ca8d6ee45d78039649512d8b5
Siena Moon <soliloquy.moon@gmail.com>
2024-09-20 01:35:44 -0500

    Implemented the basic calculator

--- app/src/main/java/com/cs407/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/cs407/basiccalculator/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.cs407.basiccalculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,46 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val firstTextField = findViewById<EditText>(R.id.firstTextField)
+        val secondTextField = findViewById<EditText>(R.id.secondTextField)
+        val additionButton = findViewById<Button>(R.id.additionButton)
+        val subtractionButton = findViewById<Button>(R.id.subtractionButton)
+        val multiplicationButton = findViewById<Button>(R.id.multiplicationButton)
+        val divisionButton = findViewById<Button>(R.id.divisionButton)
+
+        additionButton.setOnClickListener {
+            val firstNum = Integer.parseInt(firstTextField.text.toString())
+            val secondNum = Integer.parseInt(secondTextField.text.toString())
+            val intent = Intent(this, CalculatorResult::class.java)
+            intent.putExtra("RESULT_MESSAGE",
+                "Result: " + (firstNum + secondNum).toString())
+            startActivity(intent)
+        }
+        subtractionButton.setOnClickListener {
+            val firstNum = Integer.parseInt(firstTextField.text.toString())
+            val secondNum = Integer.parseInt(secondTextField.text.toString())
+            val intent = Intent(this, CalculatorResult::class.java)
+            intent.putExtra("RESULT_MESSAGE",
+                "Result: " + (firstNum - secondNum).toString())
+            startActivity(intent)
+        }
+        multiplicationButton.setOnClickListener {
+            val firstNum = Integer.parseInt(firstTextField.text.toString())
+            val secondNum = Integer.parseInt(secondTextField.text.toString())
+            val intent = Intent(this, CalculatorResult::class.java)
+            intent.putExtra("RESULT_MESSAGE",
+                "Result: " + (firstNum * secondNum).toString())
+            startActivity(intent)
+        }
+        divisionButton.setOnClickListener {
+            val firstNum = Integer.parseInt(firstTextField.text.toString())
+            val secondNum = Integer.parseInt(secondTextField.text.toString())
+            val intent = Intent(this, CalculatorResult::class.java)
+            intent.putExtra("RESULT_MESSAGE",
+                "Result: " + (firstNum / secondNum).toString())
+            startActivity(intent)
         }
     }
 }
